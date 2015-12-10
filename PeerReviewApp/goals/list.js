b95fafdb65388cbe82086b565d3568d1439bf2ec
theogjpeezy <justin.cronin01@gmail.com>
2015-12-10 13:52:37 -0500

    Initial commit

---
+++ PeerReviewApp/goals/list.js
@@ -0,0 +1,19 @@
+﻿import {inject} from "aurelia-framework";
+import {GoalData} from "./goalData";
+
+@inject(GoalData)
+export class List {
+    
+    constructor(goalData) {
+        this.goalData = goalData;
+    }
+
+    activate() {
+        this.goals = this.goalData
+                         .getAll()
+                         .filter(
+                            function(goal) {
+                                return goal.isActive; 
+                            });
+    }
+}
