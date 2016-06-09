f53e12fd21d8ae3a918ba2e1f61882d947646688
cyber-decker <cyberdecker@gmail.com>
2016-06-09 17:15:11 -0400

    Changing the report tool to display reports for a user a little better with some better formatting.

--- PeerReviewApp/feedback/feedback.js
+++ PeerReviewApp/feedback/feedback.js
@@ -16,11 +16,18 @@ export class Feedback {
             params.id = 1;
         }
         this.userData.getById(params.id).then(user => this.user = user);
-        return this.giveGoalFeedbackData.getByUserId(params.id).then(goals => this.goals = goals.filter(function(goal) {
+        this.giveGoalFeedbackData.getByUserId(params.id).then(goals => this.goals = goals.filter(function(goal) {
             if (goal.Feedback.length > 0) {
+                goal.Feedback.forEach(function(feedback) {
+                    feedback.DateTimeSubmitted = function(date) {
+                        var formatDate = new Date(date);
+                        var options = { year: "numeric", month: "long",
+                            day: "numeric" };
+                        return formatDate.toLocaleString("en-US", options);
+                    }(feedback.DateTimeSubmitted) ;
+                });
                 return goal;
             }
         }));
-        
     }
 }
